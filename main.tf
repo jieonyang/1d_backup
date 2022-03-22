7a7f0e85f456f1681940cf1c9668e684b688b922
jieonyang <7ieon.yang@gmail.com>
2022-03-22 07:28:40 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,119 @@
+module "vpc" {
+  source          = "./vpc_module"
+  cluster_name    = var.cluster_name
+}
+
+
+module "eks" {
+  source = "./eks_module"
+
+  cluster_name      = var.cluster_name
+  cluster_node_name = var.cluster_node_name
+  node_type         = var.node_type
+  node_desired_size = var.node_desired_size
+  node_max_size     = var.node_max_size
+  node_min_size     = var.node_min_size
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_id1 = module.vpc.private_subnet_id[0]
+  subnet_id2 = module.vpc.private_subnet_id[1]
+}
+
+
+resource aws_instance "bastion" {
+  ami             = var.my_ami
+  instance_type   = "t2.micro"
+  subnet_id       = module.vpc.public_subnet_id[0]
+  private_ip      = "10.0.1.10"
+  security_groups = [aws_security_group.bastion_sg.id]
+  key_name    = "MyKeyPair"
+
+  tags = {
+    Name = "bastion-server" 
+  }
+}
+
+
+resource aws_instance "admin" {
+  
+  ami             = var.my_ami
+  instance_type   = "t2.micro"
+  subnet_id       = module.vpc.private_subnet_id[0]
+  private_ip      = "10.0.10.10"  
+  security_groups = [aws_security_group.admin_sg.id]
+  key_name    = "MyKeyPair"
+  
+  user_data = <<EOF
+#cloud-boothook
+#!/bin/bash -xe
+
+### awscli install
+sudo apt-get update
+sudo apt-get -y install awscli
+
+### kubectl install
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+
+### .kube/config
+#aws eks --region "{$var.aws_region}" update-kubeconfig --name mgmt-eks-cluster
+
+EOF
+
+  tags = {
+    Name = "admin-server"
+  }
+}
+
+
+resource "aws_security_group" "bastion_sg" {
+  name        = "mgmt_bastion_sg"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "mgmt_bastion_sg"
+  }
+}
+
+resource "aws_security_group" "admin_sg" {
+  name        = "mgmt_admin_sg"
+  description = "admin server sg"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description      = "SSH from admin-server"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["10.0.1.10/32"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "mgmt_admin_sg"
+  }
+}
---
+++ vars.tf
@@ -0,0 +1,53 @@
+## Define Common variables #################################
+
+variable "aws_region" {
+  default     = null 
+  type        = string
+  description = "AWS region"
+}
+
+variable "my_ami" {
+  default     = null
+  type        = string
+  description = "EC2 ami"
+}
+
+### Define EKS variables ###################################
+variable "cluster_name" {
+  default     = "eks-cluster"
+  type        = string
+  description = "eks cluster name"
+}
+
+variable "cluster_node_name" {
+  default     = "eks-nodegroup"
+  type        = string
+  description = "eks cluster node group name"
+}
+
+variable "node_type" {
+  default     = ["t3.medium"]
+  type        = list(any)
+  description = "eks cluster node type"
+}
+
+variable "node_desired_size" {
+  default = 2
+  type    = number
+  description = "eks-node desired size"
+}
+
+variable "node_max_size" {
+  default = 2 
+  type    = number
+  description = "eks- node maxize"
+}
+
+variable "node_min_size" {
+  default = 2 
+  type    = number
+  description = "eks-node desired size"
+}
+
+
+
