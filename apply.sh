e6f57dafa61c8985581d3152b701686698782b95
jieonyang <98262144+jieonyang@users.noreply.github.com>
2022-05-13 10:26:20 +0900

    Update apply.sh

--- apply.sh
+++ apply.sh
@@ -6,13 +6,6 @@ read account
 aws configure
 region=$(aws configure get region)
 
-### aws-iam-authenticator install
-if [ ! -e aws-iam-authenticator* ] ; then
-curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.21.2/2021-07-05/bin/linux/amd64/aws-iam-authenticator
-chmod +x ./aws-iam-authenticator
-mkdir -p $HOME/bin && cp ./aws-iam-authenticator $HOME/bin/aws-iam-authenticator && export PATH=$PATH:$HOME/bin
-fi
-
 ### .kube/config
 aws eks update-kubeconfig --name mgmt-eks-cluster
 
